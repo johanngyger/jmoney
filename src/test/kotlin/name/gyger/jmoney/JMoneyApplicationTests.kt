12cae787579d9146711989d8318a4658e1b8d539
Johann Gyger <johann@gyger.name>
2017-01-24 14:19:18 +0100

    Use PowerMock to test main method.

--- src/test/kotlin/name/gyger/jmoney/JMoneyApplicationTests.kt
+++ src/test/kotlin/name/gyger/jmoney/JMoneyApplicationTests.kt
@@ -1,12 +1,24 @@
 package name.gyger.jmoney
 
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.powermock.api.mockito.PowerMockito
+import org.powermock.core.classloader.annotations.PrepareForTest
+import org.powermock.modules.junit4.PowerMockRunner
+import org.springframework.boot.SpringApplication
 
+@RunWith(PowerMockRunner::class)
+@PrepareForTest(SpringApplication::class)
 class JMoneyApplicationTests {
 
     @Test
-    @Throws(Exception::class)
+    fun testAppClass() {
+        JMoneyApplication()
+    }
+
+    @Test
     fun testMain() {
+        PowerMockito.mockStatic(SpringApplication::class.java)
         main(arrayOf<String>())
     }
 
